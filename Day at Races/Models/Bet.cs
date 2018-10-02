5189274f68bc7be7d589ad7604d6a14454535839
Marcin Lejko <marcin.lejko@gmail.com>
2018-10-02 21:35:51 +0200

    Initial_UI

--- Day at Races/Models/Bet.cs
+++ Day at Races/Models/Bet.cs
@@ -11,6 +11,7 @@ namespace Day_at_Races.Models
         public int Amount { get; set; }
         public int Dog { get; set; } // The amount of cash that was bet
         public Guy Bettor { get; set; }// The number of the dog the bet is on
+       
 
 
         public string GetDescription()
